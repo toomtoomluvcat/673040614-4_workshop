faa148aa30bba40f74fa8711f5f9912a420fbe1a
kitsada babthong <kritsada.ba@kkumail.com>
2025-07-04 15:37:45 +0700

    nice

--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello,world")
-	name := "Toom"
 	//nice to meet
-	fmt.Println(name)
+	fmt.Println("toomtam")
 	fmt.Println("nice to meet you")
 	fmt.Print("nice to meet you too toom")
 	if true {
 		fmt.Println("hello")
 	}
-	if false{
+	if false {
 		fmt.Println("world")
 	}
 }
