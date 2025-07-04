68bcaeeab598dd499a2ac1d54bc15deda6267e3a
MonochromeBearr <jidapa.na@kkumail.com>
2025-07-04 15:35:04 +0700

    make confilct

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("Hello,world")
 	name := "Toom"
+	//nice to meet
 	fmt.Println(name)
 	fmt.Println("nice to meet you")
-	fmt.Print("nice")
+	fmt.Print("nice to meet you too toom")
+	if true {
+		fmt.Println("hello")
+	}
 }
